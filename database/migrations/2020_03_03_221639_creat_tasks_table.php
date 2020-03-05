174b318fb8bbeacec80a77fa0d91613e40924622
MohammedTubji <tubji98@gmail.com>
2020-03-05 10:28:43 -0800

    secound commit

--- database/migrations/2020_03_03_221639_creat_tasks_table.php
+++ database/migrations/2020_03_03_221639_creat_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatTasksTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class CreatTasksTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string ('title');
+            $table->mediumText('body');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreatTasksTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tasks');
     }
 }
