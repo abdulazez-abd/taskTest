d8c90b897a52b8f928de99ec16d17520a82e1129
abdulazez-abd <babwaljybn11@gmail.com>
2020-03-04 22:38:02 -0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE <!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Document</title>
 </head>
 <body>
     @foreach($tasks as $task)
-    <h3> {{$task->name}}</h3>
+    <h3>{{$task->name}}</h3>
     @endforeach
 </body>
 </html>
